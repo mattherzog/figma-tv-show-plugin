0b9177e35c221992b1aa0a1a56c13affb71cf31a
mattherzog <Herzog.Matt@gmail.com>
2021-12-05 19:10:29 -0800

    Initial commit of working plugin

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Figma-TV-Show-Plugin
-Example of a Figma plugin that populates image and text layers from API
+# Read me
+## Testing out the plugin
+- Download or clone this repository
+- In Figma, select `Plugins -> Development -> Import plugin from manifest...` from the menu
+- Navigate to `manifest.json` in this repository and click 'Open'
+
+Plugin is now installed, to use it: 
+- In Figma, select `Plugins -> Development -> tv-example` from the menu.
+- When you hit 'Random' or 'Apply' it will download data for a given TV show and populate your select frames/layers. 
+- For an example check out this design example: https://www.figma.com/file/MiS3or7gZpYaD3rlaKPTBt/TV-show-plugin-example?node-id=2%3A2 You can copy/paste into your own file.
+
+## Making updates to the plugin
+Read full plugin-in instructions here: https://www.figma.com/plugin-docs/setup/
---
+++ code.js
@@ -0,0 +1,59 @@
+figma.showUI(__html__, { width: 400, height: 260 });
+figma.ui.onmessage = msg => {
+    if (msg.type === "populateShow") {
+        const layerMap = msg.data;
+        handleText(layerMap, figma.currentPage.selection[0]);
+    }
+    else if (msg.type === "populateImage") {
+        const { imageBytes, key } = msg.data;
+        handleImage(imageBytes, key, figma.currentPage.selection[0]);
+    }
+    else if (msg.type === "cancel") {
+        figma.closePlugin();
+    }
+};
+const valueForCaseInsensitiveKey = (object, caseInsensitiveKey) => {
+    return object[Object.keys(object)
+        .find(k => k.toLowerCase() === caseInsensitiveKey.toLowerCase())];
+};
+const handleText = (layerMap, node) => {
+    // if node has children, iterate and call this method again
+    // otherwise see if it's a text field
+    if ("children" in node) {
+        for (const child of node.children) {
+            handleText(layerMap, child);
+        }
+    }
+    else {
+        // check if node is text
+        if ("characters" in node) {
+            // match name to layerMap
+            const value = valueForCaseInsensitiveKey(layerMap, node.name);
+            if (value) {
+                const fontName = node.fontName;
+                figma.loadFontAsync(fontName).then(() => {
+                    node.characters = value;
+                });
+            }
+        }
+    }
+};
+const handleImage = (imageBytes, key, node) => {
+    if ("children" in node) {
+        for (const child of node.children) {
+            handleImage(imageBytes, key, child);
+        }
+    }
+    else {
+        if ("fills" in node && node.name.toLowerCase() === key.toLowerCase()) {
+            const imageRectangleNode = node;
+            const image = figma.createImage(imageBytes);
+            imageRectangleNode.fills = [{
+                type: 'IMAGE',
+                scaleMode: 'FILL',
+                imageHash: image.hash
+            }
+            ];
+        }
+    }
+};
